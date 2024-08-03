2e1bf6b8bb8ab89b3080627bd545f0b4008bdc39
jetakazono <jessica.takazono@gmail.com>
2024-08-03 02:15:29 +0100

    fix responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ async function Home() {
     const data = await fetchAnime(1);
 
     return (
-        <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
+        <main className="sm:p-16 py-16 px-8 flex flex-col gap-10 items-center">
             <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
-            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+            <section className="grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 justify-items-center">
                 {data}
             </section>
             <LoadMore />
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -25,7 +25,7 @@ function LoadMore() {
 
     return (
         <>
-            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+            <section className="grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 justify-items-center">
                 {data}
             </section>
             <section className="flex justify-center items-center w-full">
